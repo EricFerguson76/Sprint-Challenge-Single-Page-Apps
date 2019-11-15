1853ce26a9a2e3ed7dfa1093315c16eed7f8f9fe
EricFerguson76 <turtle0888@gmail.com>
2019-11-15 11:51:41 -0600

    Added to CharacterCard component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return <span>
+    <img src={props.image} alt='' />
+    <h2>{props.name}</h2>
+    <h3>{props.species}</h3>
+    <h3>{props.status}</h3>
+
+  </span>;
 }
